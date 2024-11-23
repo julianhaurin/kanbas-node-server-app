4d5d5594ba0c0a7a4550a4ec83623aa21398c4f8
Julian Haurin <julian.haurin@gmail.com>
2024-11-23 04:38:57 -0500

    add cookies

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ const sessionOptions = {
 
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
-  // NOTE: removing this fixed a session error i was having (currentUser was undefined after loggin in) *****
-  // sessionOptions.cookie = {
-  //   sameSite: "none",
-  //   secure: true,
-  //   domain: process.env.NODE_SERVER_DOMAIN,
-  // };
+  // NOTE: removing this fixed a session error i was having locally (currentUser was undefined after loggin in) *****
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
 }
 
 app.use(session(sessionOptions));
